9b30c189028dab8fd543ab311e8e68ed2bcfb8ec
oxrmr <olexandr.morar>
2024-04-25 19:23:45 +0300

    remove unused vars

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,12 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 type Mods = Record<string, boolean | string>;
 
-export const classNames = (className: string, mods: Mods = {}, additional: string[] = []) => {
+export const classNames = (
+  className: string,
+  mods: Mods = {},
+  additional: string[] = []
+) => {
   return [
     className,
     ...Object.entries(mods)
-      .filter(([key, value]) => Boolean(value))
-      .map(([key, value]) => key),
+      .filter(([_, value]) => Boolean(value))
+      .map(([key, _]) => key),
     ...additional.filter(Boolean),
   ].join(' ');
 };
