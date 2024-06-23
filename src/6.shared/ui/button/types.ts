52bdb737396176d7fcf713ddcb008b7c718a3f9d
oxrmr <olexandr.morar@gmail.com>
2024-06-24 00:58:52 +0300

    #

--- src/6.shared/ui/button/types.ts
+++ src/6.shared/ui/button/types.ts
@@ -1,10 +1,8 @@
-// eslint-disable-next-line react-refresh/only-export-components
 export enum ButtonThemes {
   LIGHT = "lightButton",
   GREY = "greyButton",
 }
 
-// eslint-disable-next-line react-refresh/only-export-components
 export enum ButtonSizes {
   SM = "smallButton",
   MD = "mediumButton",
